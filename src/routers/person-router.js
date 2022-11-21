577049ace2dd5267294672919ad12781f92ebe66
Igor Ming <igor.ming@gmail.com>
2022-11-20 22:58:12 -0300

    create put and delete endpoints

--- src/routers/person-router.js
+++ src/routers/person-router.js
@@ -12,3 +12,27 @@ router.post('/', async (req, res) => {
   const person = await personRepository.createAndSave(req.body);
   res.send(person);
 });
+
+router.put('/:id', async (req, res) => {
+  const { body, params } = req;
+  const person = await personRepository.fetch(params.id);
+
+  person.firstName = body.firstName ?? null;
+  person.lastName = body.lastName ?? null;
+  person.age = body.age ?? null;
+  person.verified = body.verified ?? null;
+  person.location = body.location ?? null;
+  person.locationUpdated = body.locationUpdated ?? null;
+  person.skills = body.skills ?? null;
+  person.personalStatement = body.personalStatement ?? null;
+
+  await personRepository.save(person);
+
+  res.send(person);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  personRepository.remove(id);
+  res.send({ id });
+});
