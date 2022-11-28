2ef06080a3117dc1afaec0e74f5b7f33c309fdf7
Igor Ming <igor.ming@gmail.com>
2022-11-28 01:07:39 -0300

    comment swagger to get rid of it

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import 'dotenv/config';
 
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import personRouter from './src/routers/person-router';
+// import swaggerUi from 'swagger-ui-express';
+// import YAML from 'yamljs';
+import peopleRouter from './src/routers/people-router';
 import searchRouter from './src/routers/search-router';
 
 /* create an express app and use JSON */
 const app = new express();
 app.use(express.json());
 
-app.use('/person', personRouter);
+app.use('/people', peopleRouter);
 app.use('/search', searchRouter);
 
 /* set up swagger in the root */
-const swaggerDocument = YAML.load('api.yaml');
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// const swaggerDocument = YAML.load('api.yaml');
+// app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* start the server */
 app.listen(8080);
